1256cdf1ec6250828bb2f9ea24abfde2588c5d5e
ProfessorCalculus123 <dmrosini@gmail.com>
2026-04-24 11:15:45 +1000

    Update Power Ed site

--- myfirstphp.php
+++ myfirstphp.php
@@ -9,9 +9,9 @@
     <h1>PHP Variables, arrays and operators </h1>
 
 <?php
-    $marks = array (85, 85, 95);  // declare and initialise array
-    $marks[1] = 90;               // modify second element
-    $ave = ($marks[0] + $marks[1] + $marks[2])/3;  //Compute Average
+    $marks = array (85, 85, 95);  
+    $marks[1] = 90;              
+    $ave = ($marks[0] + $marks[1] + $marks[2])/3;  
     
     if($ave >= 50)
         $status = "PASSED";
