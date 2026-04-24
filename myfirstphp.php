46d309e9ca93aef1da7aff7268e2a1f5ee36919b
ProfessorCalculus123 <dmrosini@gmail.com>
2026-04-24 11:12:38 +1000

    Update Power Ed site

--- myfirstphp.php
+++ myfirstphp.php
@@ -1,18 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>Using PHP Variables, arrays and operators</title>
+    </head>
+<body>
+
+    <h1>PHP Variables, arrays and operators </h1>
+
 <?php
+    $marks = array (85, 85, 95);  // declare and initialise array
+    $marks[1] = 90;               // modify second element
+    $ave = ($marks[0] + $marks[1] + $marks[2])/3;  //Compute Average
+    
+    if($ave >= 50)
+        $status = "PASSED";
+    else
+        $status = "FAILED";
 
-$marks = [85, 85, 95];
-
-$marks[1] = 90;
-
-
-$ave = array_sum($marks) / count($marks);
-
-if ($ave >= 50) {
-    $status = "PASSED";
-} else {
-    $status = "FAILED";
-}
-
-echo "Average Mark: " . $ave . "<br>";
-echo "Status: " . $status;
+    echo "<p>The average score is $ave. You $status.</p>";
 ?>
+
+</body>
+</html>
