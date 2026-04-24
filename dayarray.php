4c9d1f9d8f941c061f644340345c1d448cc277ca
ProfessorCalculus123 <dmrosini@gmail.com>
2026-04-24 11:19:07 +1000

    Update Power Ed site

---
+++ dayarray.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>Using PHP Variables, arrays and operators</title>
+    </head>
+<body>
+
+    <h1>PHP Variables, arrays and operators </h1>
+
+<?php
+    $days = [Monday, Tuesday, Wednesday, Thursday,Friday, Saturday, Sunday]
+    echo "<p>$days</p>";
+?>
+
+</body>
+</html>
